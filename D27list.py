8c0ed8c64dd6ddfca3b1bf830fc6fce5c7ad180f
007annmol <annusmaryvincent@gmail.com>
2022-06-05 19:37:07 +0530

    D28

---
+++ D27list.py
@@ -0,0 +1,7 @@
+l=["Ann",47,13.9,"Eliphent"]
+u=["python", "Stranger", "Things", "Season",4]
+
+print(l+u)
+l[3]=55
+l=l+['22']
+print (l)
---
+++ D28circle.py
@@ -0,0 +1,8 @@
+import turtle as t
+
+t.color('red')
+t.speed(11)
+for i in range(50,102):
+    t.circle(i*2)
+
+t.bye()
